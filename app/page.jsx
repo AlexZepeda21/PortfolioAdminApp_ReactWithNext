474b81f166370f55658054f8c4f5e2a24ecf6c19
AlexZepeda21 <manuelzepedagr@gmail.com>
2025-04-28 16:21:36 -0600

    Login básico con diseño.

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,26 @@
 import Link from "next/link"
+import "../styles/login.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+
 export default function Login() {
     return (
-        <html>
+        <html lang="es">
             <body>
-                <button>
-                    <Link href="../src/">
-                        Ir a proyectos
-                    </Link>
-                </button>
+                <main className="container-main d-flex justify-content-center align-items-center min-vh-100">
+
+                    <form action="" className="form-login sectionLogin">
+
+                        <label htmlFor="email" className="form-label">Ingrese su correo</label>
+                        <input type="email" name="email" id="email" className="form-control" placeholder="email@hotmail.com" />
+
+                        <label htmlFor="password" className="form-label mt-3">Ingrese su contraseña</label>
+                        <input type="password" name="password" id="password" className="form-control" placeholder="********" />
+
+                        <button type="submit" className="btn-login mt-4">Enviar</button>
+
+                    </form>
+
+                </main>
             </body>
         </html>
     );
